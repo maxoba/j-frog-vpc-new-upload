6dcc8206298daf12f42105942aa9152fd5c5ae1c
your name <your email>
2026-04-04 18:59:28 -0600

    fix: 3

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -107,7 +107,7 @@ resource "aws_route" "private_nat_gateway" {
   depends_on = [aws_route_table.private]
 }
 
-##9. Private route association
+#9. Private route association
 resource "aws_route_table_association" "private" {
   count = length(var.private_cidr)
 
@@ -117,7 +117,7 @@ resource "aws_route_table_association" "private" {
   depends_on = [aws_route.private_nat_gateway, aws_subnet.private]
 }
 
-##10. Public route association
+#10. Public route association
 resource "aws_route_table_association" "public" {
   count = length(var.public_cidr)
 
